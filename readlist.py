50655b3a089a834fb19d1a6b56d24b734b4f1eb2
Kimmo Heikkinen <kimmo.heikkinen@verkkokauppa.com>
2019-08-06 18:10:46 +0300

    Added a simple list reader

---
+++ readlist.py
@@ -0,0 +1,25 @@
+def main():
+    f = open("orklist.txt", "r")
+    lines = f.readlines()
+    f.close()
+
+    lines = [x.strip() for x in lines]
+    orks = []
+    ork = {}
+    for line in lines:
+        if (line == '-'):
+            orks.append(ork)
+            ork = {}
+            continue
+        split = line.split(':')
+        key = split[0]
+        value = split[1].strip()
+        if (key == 'Model'):
+            ork[key] = value
+        else:
+            if (key not in ork.keys()):
+                ork[key] = []
+            ork[key].append(value)
+    orks.append(ork)
+
+    return orks
